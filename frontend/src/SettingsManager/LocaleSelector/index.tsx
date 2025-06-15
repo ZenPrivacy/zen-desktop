100e2f003e13362af9c70719c468fff7df4ba9c7
Cornelius Carstens <cornelius.carstens@me.com>
2025-06-15 16:00:32 +0200

    added zh-CN

--- frontend/src/SettingsManager/LocaleSelector/index.tsx
+++ frontend/src/SettingsManager/LocaleSelector/index.tsx
@@ -14,6 +14,7 @@ const items: LocaleItem[] = [
   { value: 'de-DE', label: 'Deutsch' },
   { value: 'kk-KZ', label: 'Қазақша' },
   { value: 'ru-RU', label: 'Русский' },
+  { value: 'zh-CN', label: 'Chinese' },
 ];
 
 export function LocaleSelector() {
