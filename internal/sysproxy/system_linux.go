018ae60211d193e61bb91a099647b9db5b79b423
khalid <ga.lid2002.km@gmail.com>
2025-08-16 19:43:29 +0100

    feat: add kde system proxy support

--- internal/sysproxy/system_linux.go
+++ internal/sysproxy/system_linux.go
@@ -2,6 +2,7 @@ package sysproxy
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -10,11 +11,65 @@ var (
 	platformSpecificExcludedHosts []byte
 )
 
+func detectDesktopEnvironment() string {
+	desktop := os.Getenv("XDG_CURRENT_DESKTOP")
+
+	if strings.ToLower(desktop) == "kde" {
+		return "kde"
+	}
+
+	if strings.Contains(strings.ToLower(desktop), "gnome") {
+		return "gnome"
+	}
+
+	return ""
+}
+
 func setSystemProxy(pacURL string) error {
-	if !binaryExists("gsettings") {
-		// TODO: add support for other desktop environments
+	desktop := detectDesktopEnvironment()
+
+	// TODO: add support for other desktop environments
+	switch desktop {
+	case "kde":
+		return setKDEProxy(pacURL)
+
+	case "gnome":
+		return setGnomeProxy(pacURL)
+
+	default:
 		return ErrUnsupportedDesktopEnvironment
 	}
+}
+
+func setKDEProxy(pacURL string) error {
+	var kwriteconfig string
+
+	if binaryExists("kwriteconfig6") {
+		kwriteconfig = "kwriteconfig6"
+	}
+
+	if binaryExists("kwriteconfig5") {
+		kwriteconfig = "kwriteconfig5"
+	}
+
+	commands := [][]string{
+		{kwriteconfig, "--file", "kioslaverc", "--group", "Proxy Settings", "--key", "ProxyType", "2"},
+		{kwriteconfig, "--file", "kioslaverc", "--group", "Proxy Settings", "--key", "Proxy Config Script", pacURL},
+		{kwriteconfig, "--file", "kioslaverc", "--group", "Proxy Settings", "--key", "ReversedException", "false"},
+	}
+
+	for _, command := range commands {
+		cmd := exec.Command(command[0], command[1:]...)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("run KDE proxy command %q: %v (%q)", strings.Join(command, " "), err, out)
+		}
+	}
+
+	return nil
+}
+
+func setGnomeProxy(pacURL string) error {
 
 	commands := [][]string{
 		{"gsettings", "set", "org.gnome.system.proxy", "autoconfig-url", pacURL},
@@ -25,22 +80,54 @@ func setSystemProxy(pacURL string) error {
 		cmd := exec.Command(command[0], command[1:]...) // #nosec G204
 		out, err := cmd.CombinedOutput()
 		if err != nil {
-			return fmt.Errorf("run system proxy command %q: %v (%q)", strings.Join(command, " "), err, out)
+			return fmt.Errorf("run GNOME proxy command %q: %v (%q)", strings.Join(command, " "), err, out)
 		}
 	}
 	return nil
 }
 
 func unsetSystemProxy() error {
-	if !binaryExists("gsettings") {
-		// TODO: add support for other desktop environments
+
+	desktop := detectDesktopEnvironment()
+	switch desktop {
+	case "kde":
+		return unsetKDEProxy()
+
+	case "gnome":
+		return unsetGnomeProxy()
+
+	default:
 		return ErrUnsupportedDesktopEnvironment
 	}
+}
+
+func unsetKDEProxy() error {
+
+	var kwriteconfig string
+
+	if binaryExists("kwriteconfig6") {
+		kwriteconfig = "kwriteconfig6"
+	}
+
+	if binaryExists("kwriteconfig5") {
+		kwriteconfig = "kwriteconfig5"
+	}
+
+	cmd := exec.Command(kwriteconfig, "--file", "kioslaverc", "--group", "Proxy Settings", "--key", "ProxyType", "0")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("unset KDE proxy: %v (%q)", err, out)
+	}
+
+	return nil
+}
+
+func unsetGnomeProxy() error {
 
 	cmd := exec.Command("gsettings", "set", "org.gnome.system.proxy", "mode", "none")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("unset system proxy: %v (%q)", err, out)
+		return fmt.Errorf("unset GNOME proxy: %v (%q)", err, out)
 	}
 
 	return nil
