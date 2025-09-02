5cfe68734fe1880034c9fe89a442a24b116433f3
Ansar Smagulov <ansar@anfragment.net>
2025-09-02 23:44:59 +0500

    Remove unnecessary :scope prefixing in Has constructor; add :has() test suite

--- extended-css/src/parse/extendedPseudoClasses/has.ts
+++ extended-css/src/parse/extendedPseudoClasses/has.ts
@@ -10,9 +10,6 @@ export class Has implements Step {
   constructor(arg: string) {
     // arg may include multiple selectors in a selector list.
     this.runners = arg.split(',').map((selector) => {
-      if (!selector.startsWith(':scope')) {
-        selector = ':scope ' + selector;
-      }
       return new QueryRunner(parse(selector));
     });
   }
