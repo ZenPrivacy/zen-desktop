e97d14885e4ebb905990f17efd2bc9aa9090827a
Ansar Smagulov <ansar@anfragment.net>
2025-09-08 22:52:03 +0500

    Refactor SubsequentSibling to simplify/improve performance of sibling selection logic

--- extended-css/src/parse/combinators/subsequentSibling.ts
+++ extended-css/src/parse/combinators/subsequentSibling.ts
@@ -9,22 +9,11 @@ export class SubsequentSibling implements Step {
   run(input: Element[]) {
     const result = new Set<Element>();
 
-    for (const element of input) {
-      const parent = element.parentElement;
-
-      if (!parent) continue;
-
-      let foundCurrent = false;
-
-      for (const child of Array.from(parent.children)) {
-        if (child === element) {
-          foundCurrent = true;
-          continue;
-        }
-
-        if (foundCurrent) {
-          result.add(child);
-        }
+    for (const el of input) {
+      let sib = el.nextElementSibling;
+      while (sib) {
+        result.add(sib);
+        sib = sib.nextElementSibling;
       }
     }
 
