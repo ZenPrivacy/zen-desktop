35ad8226c4ab4e878fdf7a67df7928efef243552
Lukas Zapletal <lzap+git@redhat.com>
2025-09-24 07:25:47 +0200

    Refactor to use benchmark and add req test

--- internal/ruletree/ruletree_test.go
+++ internal/ruletree/ruletree_test.go
@@ -4,34 +4,24 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
 	"runtime"
+	"runtime/pprof"
 	"testing"
 
 	"github.com/ZenPrivacy/zen-desktop/internal/ruletree"
 )
 
-func TestProfileHeap(t *testing.T) {
-	if testing.Short() || os.Getenv("RULETREE_PROFILE") != "1" {
-		t.Skip("skipping heap profile (set RULETREE_PROFILE=1 to run)")
-	}
-
-	var m runtime.MemStats
-
-	runtime.GC()
-	runtime.ReadMemStats(&m)
-	t.Logf("Before: HeapAlloc=%s HeapSys=%s NumGC=%d\n",
-		humanizeBytes(m.HeapAlloc),
-		humanizeBytes(m.HeapSys),
-		m.NumGC)
-
+func loadTree() (*ruletree.RuleTree[*spyData], error) {
 	tree := ruletree.NewRuleTree[*spyData]()
 
 	for _, filename := range []string{"testdata/easylist.txt", "testdata/easyprivacy.txt"} {
 		data, err := os.ReadFile(filename)
 		if err != nil {
-			t.Fatalf("read %s: %v", filename, err)
+			return nil, fmt.Errorf("read %s: %v", filename, err)
 		}
 
 		scanner := bufio.NewScanner(bytes.NewReader(data))
@@ -44,23 +34,81 @@ func TestProfileHeap(t *testing.T) {
 
 			err := tree.Add(line, &spyData{})
 			if err != nil {
-				t.Errorf("add rule %q: %v", line, err)
+				return nil, fmt.Errorf("add rule %q: %v", line, err)
 			}
 		}
 
 		if err := scanner.Err(); err != nil {
-			t.Fatalf("scan %s: %v", filename, err)
+			return nil, fmt.Errorf("scan %s: %v", filename, err)
 		}
 	}
+	return tree, nil
+}
 
-	defer runtime.KeepAlive(tree)
+func BenchmarkLoadTree(b *testing.B) {
+	for b.Loop() {
+		tree, err := loadTree()
+		if err != nil {
+			b.Fatalf("load tree: %v", err)
+		}
+		runtime.KeepAlive(tree)
+	}
+
+	b.ReportAllocs()
+}
+
+var rnd = rand.New(rand.NewSource(1))
+
+func rndStr(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rnd.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func genRequest() *http.Request {
+	url, _ := url.Parse(fmt.Sprintf("https://%s.example.com/%s", rndStr(10), rndStr(20)))
+	return &http.Request{
+		Method: "GET",
+		URL:    url,
+		Host:   "example.com",
+	}
+}
+
+func writeHeapProfile(b *testing.B, name string) {
+	f, err := os.Create(name)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer f.Close()
 
 	runtime.GC()
-	runtime.ReadMemStats(&m)
-	t.Logf("After: HeapAlloc=%s HeapSys=%s NumGC=%d\n",
-		humanizeBytes(m.HeapAlloc),
-		humanizeBytes(m.HeapSys),
-		m.NumGC)
+	if err := pprof.WriteHeapProfile(f); err != nil {
+		b.Fatal(err)
+	}
+}
+
+func BenchmarkMatch(b *testing.B) {
+	checkStr := rndStr(5)
+	if checkStr != "XVlBz" {
+		b.Fatalf("random generator seed mismatch: %s", checkStr)
+	}
+
+	tree, err := loadTree()
+	if err != nil {
+		b.Fatalf("load tree: %v", err)
+	}
+
+	writeHeapProfile(b, "ruletree_test_heap_before.pprof")
+	for b.Loop() {
+		tree.FindMatchingRulesReq(genRequest())
+	}
+	writeHeapProfile(b, "ruletree_test_heap_after.pprof")
+	runtime.KeepAlive(tree)
+
+	b.ReportAllocs()
 }
 
 type spyData struct {
@@ -78,18 +126,4 @@ func (s *spyData) ShouldMatchReq(*http.Request) bool {
 func (s *spyData) ParseModifiers(modifiers string) error {
 	s.modifiers = modifiers
 	return nil
-}
-
-func humanizeBytes(b uint64) string {
-	const unit = 1024
-	if b < unit {
-		return fmt.Sprintf("%d B", b)
-	}
-	div, exp := unit, 0
-	for n := b / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.1f %ciB",
-		float64(b)/float64(div), "KMGTPE"[exp])
 }
